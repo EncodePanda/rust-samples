97bd226d4e33d26f7057372e27b06b2eb89e32ee
EncodePanda <paul.szulc@gmail.com>
2026-02-15 21:25:16 +0100

    Fix doc comment

--- src/waitwhat/mod.rs
+++ src/waitwhat/mod.rs
@@ -1,6 +1,6 @@
-//! This module contains examples from presentation 'wait, why (??): Surprising
-//! corners of Rust - part 1' https://www.youtube.com/watch?v=8Q3xKQGEDKA&t=3925s
-
+//! This module contains examples from the presentation 'wait, why (??):
+//! Surprising corners of Rust - part 1'
+//! <https://www.youtube.com/watch?v=8Q3xKQGEDKA&t=3925s>
 pub mod typealiases;
 pub mod filtermap;
 pub mod just;
